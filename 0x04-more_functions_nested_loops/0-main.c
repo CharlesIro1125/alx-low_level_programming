1c08df6c89a0cb89d7eb81cad9eff23ce4cd612e
CharlesIro1125 <irocharles22@gmail.com>
2022-06-10 18:58:00 -0700

    update

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -4,13 +4,13 @@
 /**
  * main - check the code.
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 
 int main(void)
 {
 	char c;
-	
+
 	c = 'A';
 	printf("%c: %d\n", c, _isupper(c));
 	c = 'a';
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers 
+ * print_numbers - prints the numbers
  * from 0 to 9
+ *
+ * description: prints the digit from
+ * 0 to 9
+ * return: 0
  *
  */
 int print_numbers(void)
 {
 	int i;
 
-	for (i = '0';i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		_putchar(i);
 	}
