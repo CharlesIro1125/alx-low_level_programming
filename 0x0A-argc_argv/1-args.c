ac06b92ba921a6f7ddc100a3bf6ad1d6c0941163
CharlesIro1125 <irocharles22@gmail.com>
2022-06-24 20:29:19 -0700

    print the number of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 /**
  * main - prints out the name of the program
- * @argc: the number of arguments passed
+ * @agrc: the number of arguments passed
  * @argv: a char pointer to the passed arguments
  * Return: an int indicating success
  */
@@ -12,6 +12,6 @@ int main(int agrc, char __attribute__ ((unused)) *argv[])
 	int i;
 
 	i = agrc - 1;
-	printf("%d\n",i);
+	printf("%d\n", i);
 	return (0);
 }
