3982e4ded72166600bd303524d58b69e8efc21d5
CharlesIro1125 <irocharles22@gmail.com>
2022-06-25 03:18:14 -0700

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add numbers
+ * @argc: the number of arguments passed
+ * @argv: a char pointer to the passed arguments
+ * Return: an int indicating success
+ */
+int main(int argc, char *argv[])
+{
+	char *endptr;
+	int i = argc - 1, j = 1;
+	long int num1 = 0, val;
+	char error[] = "Error";
+
+	for (j = 1; j <= i; j++)
+	{
+		errno = 0;
+		val = strtol(*(argv + j), &endptr, 10);
+		if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+				|| (errno != 0 && val == 0) || *endptr != '\0')
+		{
+			printf("%s\n", error);
+			return (1);
+		}
+		num1 += val;
+		if (j == i)
+		{
+			printf("%ld\n", num1);
+		}
+	}
+	if (!(argc - 1))
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+	return (0);
+}
