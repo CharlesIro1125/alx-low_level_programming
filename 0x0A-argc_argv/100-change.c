90792962b92ebf3001e6aa4c5b8c8e3f20f080b4
CharlesIro1125 <irocharles22@gmail.com>
2022-06-25 05:14:14 -0700

    change count

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,35 +18,22 @@ int main(int argc, char *argv[])
 	}
 	cent = atoi(argv[1]);
 	val = cent;
-
 	while (cent > 0)
 	{
 		if (cent >= 25)
-		{
 			cent -= 25;
-		}
 		else if (cent >= 10)
-		{
 			cent -= 10;
-		}
 		else if (cent >= 5)
-		{
 			cent -= 5;
-		}
 		else if (cent >= 2)
-		{
 			cent -= 2;
-		}
 		else if (cent >= 1)
-		{
 			cent -= 1;
-		}
 		coin += 1;
 	}
 	if (val < 0)
-	{
 		printf("%d\n", 0);
-	}
 	else if (val > 0)
 		printf("%d\n", coin);
 	return (0);
