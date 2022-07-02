1047609fc3f08aebbf16915ce5bb347342c0e8b6
CharlesIro1125 <irocharles22@gmail.com>
2022-07-02 13:34:51 -0700

    concatenate

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *str_conca - concatenate two string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
+ */
+char *str_conca(char *s1, char *s2)
+{
+	char *tar;
+	int i, p, k;
+
+	k = 0;
+	i = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (*(s1 + i) != '\0')
+		i++;
+	while (*(s2 + k) != '\0')
+		k++;
+	tar = (char *)malloc(sizeof(char) * (i + k + 1));
+	if (tar == NULL)
+		return (NULL);
+	for (p = 0; p < (i + k); p++)
+	{
+		if (p < i)
+			*(tar + p) = *(s1 + p);
+		else
+			*(tar + p) = *(s2 + (p - i));
+	}
+	return (tar);
+}
