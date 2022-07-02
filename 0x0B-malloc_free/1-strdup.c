9304825f16299446176d88764e24081102386374
CharlesIro1125 <irocharles22@gmail.com>
2022-07-02 10:11:43 -0700

    string duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup - a pointer that creates array
+ * and initializes it string
+ * @str: the initialized string
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	char *tar;
+	int i;
+	int p;
+
+	i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (*(str + i) != '\0')
+		i++;
+	tar = (char *)malloc(sizeof(char) * i);
+	if (tar == NULL)
+		return (NULL);
+	for (p = 0; p < i; p++)
+	{
+		*(tar + i) = *(str + i);
+	}
+	return (tar);
+}
