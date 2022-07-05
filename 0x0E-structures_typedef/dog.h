345b987e3d9d81d1b69f45d590a3e45558cd3ae2
CharlesIro1125 <irocharles22@gmail.com>
2022-07-04 17:00:47 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef DOG
 #define DOG
+/**
+ * struct dog - data type
+ * @name: char pointer
+ * @age: float
+ * @owner: char pointer
+ */
 struct dog
-{	
+{
 	char *name;
 	float age;
 	char *owner;
