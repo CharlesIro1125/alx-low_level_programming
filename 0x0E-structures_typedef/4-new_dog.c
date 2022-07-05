17278157ba174eb07a0fef628405732a09ba4ecc
CharlesIro1125 <irocharles22@gmail.com>
2022-07-05 07:00:13 -0700

    typedef function

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * new_dog - struct pointer
+ * @age: int
+ * @name: char pointer
+ * @owner: char pointer
+ * Return: dog_t type
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d_dog;
+	dog_t y_dog;
+
+	if (name == NULL || owner == NULL || age == 0.0)
+		return (NULL);
+	y_dog.name = name;
+	y_dog.age = age;
+	y_dog.owner = owner;
+	d_dog = &y_dog;
+	if (d_dog == NULL)
+		return (NULL);
+	return (d_dog);
+}
