9f906f32657e93000cca18cb182b5ca6353924cd
CharlesIro1125 <irocharles22@gmail.com>
2022-07-08 05:58:06 -0700

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - a variadic function of sum
+ * @separator: the first argument
+ * @n: the second arg
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list var;
+	unsigned int j, count = 0;
+	int sum = 0;
+
+	if (*separator == NULL)
+		return;
+	va_start(var, n);
+	for (j = va_arg(var, int); count < n; count++)
+	{
+		printf("%d", j);
+		if (count < n - 1)
+			printf("%c ", *separator);
+		j = va_arg(var, int);
+	}
+	printf('\n');
+	va_end(var);
+}
