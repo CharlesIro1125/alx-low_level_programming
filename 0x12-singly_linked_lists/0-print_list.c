840d0b2e93bd31a0b70553bc23beb09393f2af84
CharlesIro1125 <irocharles22@gmail.com>
2022-07-14 08:10:41 -0700

    print elements of list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,19 @@
 #include "lists.h"
 /**
+ * getlen - function to return the length
+ * of a string.
+ * @s: pointer to char
+ * Return: the length of the string
+ */
+unsigned int getlen(char *s)
+{
+	unsigned int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
  * print_list - prints out the elements
  * of the list
  * @h: pointer to struct list_s
