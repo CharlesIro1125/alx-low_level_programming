c202ce2db96081ae52036c03e3b84a85a9bd4cfb
CharlesIro1125 <irocharles22@gmail.com>
2022-07-14 15:50:51 -0700

    free memory

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,5 @@
 #include "lists.h"
 /**
- * getlen - function to return the length
- * of a string.
- * @s: pointer to char
- * Return: the length of the string
- */
-unsigned int getlen(char *s)
-{
-	unsigned int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i);
-}
-/**
  * free_list - frees the elements of the list
  * @h: pointer to struct list_s
  * Return: void
