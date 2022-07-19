e10cd2f637690deed51c4574e4e982b280045c7a
CharlesIro1125 <irocharles22@gmail.com>
2022-07-19 05:24:01 -0700

    print the integer value

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -13,9 +13,4 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-size_t list_len(const list_t *h);
-void free_list(list_t *h);
-size_t print_list(const list_t *h);
 #endif
