0f47eeeffa3b1adcb9d478717138569f57d6144e
CharlesIro1125 <irocharles22@gmail.com>
2022-07-20 11:38:57 -0700

    free nodes

---
+++ 0x13-more_singly_linked_lists/test/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * free_listint2 - free the struct element
+ * @head: pointer to struct listint
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *prev;
+	listint_t *first = *head;
+
+	if (head == NULL)
+	{
+		printf("Freed !\n");
+		return;
+	}
+	if (*head == NULL)
+	{
+		return;
+	}
+	while (*head != NULL)
+	{
+		prev = *head;
+		*head = (*head)->next;
+		free(prev);
+	}
+	*head = first;
+	*head = NULL;
+}
