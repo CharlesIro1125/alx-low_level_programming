cb388a49cc01b023f67456890d591f87a6aa7321
CharlesIro1125 <irocharles22@gmail.com>
2022-07-21 00:08:24 -0700

    add node at nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,6 +25,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 		*head = (*head)->next;
 	}
+	if (*head == NULL)
+		return (NULL);
 	if (i != idx)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
