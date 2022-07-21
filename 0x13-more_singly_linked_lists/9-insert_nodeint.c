b98a7f34a2a8dd8a28c701740b32621b1e0f7ebc
CharlesIro1125 <irocharles22@gmail.com>
2022-07-21 00:12:27 -0700

    add node at nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,6 +45,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = (*head)->next;
 	(*head)->next = new;
-	*head = first;
 	return (*head);
 }
