8510823cf911ebf5db7e33a432e498ffaa599608
CharlesIro1125 <irocharles22@gmail.com>
2022-07-22 17:48:48 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,8 +11,8 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int h, p;
 	char *bits;
 
-	if (n == '\0')
-		return ('0');
+	if (n == '\0' || index == '\0') 
+		return (-1);
 	h = n;
 	while (h)
 	{
