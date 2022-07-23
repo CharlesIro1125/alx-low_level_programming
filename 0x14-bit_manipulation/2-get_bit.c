960faca58b3ef2ac60a382d19eb0b4cffff45457
CharlesIro1125 <irocharles22@gmail.com>
2022-07-22 17:25:15 -0700

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * get_bit - get the bit at position
+ * @n: long int
+ * @index: index of bit to get
+ * Return: the bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int j = 0, k, res;
+	unsigned long int h, p;
+	char *bits;
+
+	if (n == '\0')
+		return ('0');
+	h = n;
+	while (h)
+	{
+		h = h >> 1;
+		j++;
+	}
+	j--;
+	bits = malloc(sizeof(char) * (j + 2));
+	if (bits == NULL)
+		return (-1);
+	for (k = j; k >= 0; k--)
+	{
+		p = n >> k;
+		if (p & 1)
+			bits[j - k] = '1';
+		else
+			bits[j - k] = '0';
+	}
+	bits[j + 1] = '\0';
+	res = bits[j - index] - '0';
+	free(bits);
+	return (res);
+}
