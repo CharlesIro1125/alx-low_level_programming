c8688d4bcb048b32492d5cb3874177597c154282
Charles Obinna Iro <irocharles22@gmail.com>
2022-09-28 15:36:06 -0700

    hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,18 +2,19 @@
 
 /**
  * hash_table_create - a hash table
- * size: size of the hash table
+ * @size: size of the hash table
  * Return:a pointer to the head of a link list
  *
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int s = size;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
-	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
+	table->size = s;
+	table->array = malloc(sizeof(hash_node_t *) * s);
 	return (table);
 }
