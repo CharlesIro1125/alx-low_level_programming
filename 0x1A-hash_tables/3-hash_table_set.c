a2f2767fec4f9cb071ac4e1f5cd6beac170fa1dd
Charles Obinna Iro <irocharles22@gmail.com>
2022-09-30 06:49:44 -0700

    hash table set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,7 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	while (head != NULL)
 	{
-		if (head->key == temp->key)
+		if (strcmp(head->key, key) == 0)
 		{
 			free(head->value);
 			head->value = temp->value;
