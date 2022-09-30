cf7abb4fbd29f0689f0966a6b3dbfe57804f8b3c
Charles Obinna Iro <irocharles22@gmail.com>
2022-09-30 04:22:16 -0700

    hash table set

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,27 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 	unsigned long int s = size;
+	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
+	{
+		free(table);
 		return (NULL);
+	}
 	table->size = s;
 	table->array = malloc(sizeof(hash_node_t *) * s);
+	if (table->array == NULL)
+	{
+		free(table->array);
+		return (NULL);
+	}
+	i = s;
+	while (i)
+	{
+		table->array[i] = NULL;
+		i--;
+	}
+	table->array[i] = NULL;
 	return (table);
 }
