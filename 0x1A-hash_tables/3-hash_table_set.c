39e450be45104ecc55cad86fba16e5cb6ab0c80f
Charles Obinna Iro <irocharles22@gmail.com>
2022-09-30 00:12:55 -0700

    hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,44 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - The hash table set
+ * @ht: hash table struct
+ * @key: key to the hash map
+ * @value: The value to be writen in the map
+ * Return: int
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t **nodes;
+	hash_node_t *head;
+	hash_node_t *temp;
+	unsigned long int index;
+
+	if (*key == '\0' || key == NULL)
+		return (0);
+	if (ht == NULL)
+		return (0);
+	nodes = ht->array;
+	index = key_index((unsigned char *)key, ht->size);
+	temp = malloc(sizeof(hash_node_t));
+	if (temp == NULL)
+	{
+		free(temp);
+		return (0);
+	}
+	temp->key = (char *)key;
+	temp->value = (char *)value;
+	head = nodes[index];
+	if (head == NULL)
+	{
+		temp->next = NULL;
+		nodes[index] = temp;
+		return (1);
+	}
+	else
+	{
+		temp->next = head;
+		nodes[index] = temp;
+	}
+	return (1);
+}
+
