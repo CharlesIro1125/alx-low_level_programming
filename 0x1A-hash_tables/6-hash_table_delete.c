82de7a914622ad3b1b8d29316dcdc16a889c098f
Charles Obinna Iro <irocharles22@gmail.com>
2022-10-02 02:39:56 -0700

    delete nodes

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - The hash table to delete
+ * map
+ * @ht: hash table struct
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t **nodes;
+	hash_node_t *head, *temp;
+	unsigned long int i;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	nodes = ht->array;
+	if (nodes == NULL)
+	{
+		free(nodes);
+		free(nodes);
+		return;
+	}
+	for (i = 0; i < ht->size; i++)
+	{
+		head = nodes[i];
+		while (head != NULL)
+		{
+			free(head->key);
+			free(head->value);
+			temp = head;
+			head = head->next;
+			free(temp->next);
+			free(temp);
+		}
+	}
+	free(nodes);
+	free(ht);
+}
